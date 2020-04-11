a8445b44d7aa9577b16e4dbd1e6ab9098d673fee
arnmatu <arnas.matusevicius@gmail.com>
2020-04-11 15:39:44 -0700

    14_complete

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs'); // require fs module fs - 'file system'
 const http = require('http');
+const url = require('url');
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -30,5 +31,36 @@ const http = require('http');
 //     });
 // });
 
-
+//////////////////////////////////////////////
 //HTTP SERVER//
+/////////////////////////////////////////////////
+
+//function is blocking but only executed once
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview');
+    } else if (pathName ==='/product'){
+        res.end('This is the product');
+    } else if ('API'){
+       res.writeHead(200,{'Content-type' :'application/json'});
+       res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header' : 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
+    
+});
+
+server.listen(8000,'127.0.0.1',() => {
+    console.log(`Listening to requests on port 8000`);
+});
